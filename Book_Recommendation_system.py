ae73ca5e6200fa76051f5b1c5c96034958d958c6
maxto5 <mathewdrucker@gmail.com>
2022-07-06 14:52:55 +0300

    Add files via upload
    added the content-based filtering for book title similarity

--- Book_Recommendation_system.py
+++ Book_Recommendation_system.py
@@ -15,16 +15,15 @@
 
 import numpy as np
 import pandas as pd
-import scipy
 
-books = pd.read_csv('book.csv', dtype='unicode')
+books = pd.read_csv('Books.csv', dtype='unicode')
 users = pd.read_csv('Users.csv')
 ratings = pd.read_csv('Ratings.csv')
 
 print('drop 0 rated books')
 ratings = ratings[ratings['Book-Rating'] > 0]
 
-#books.drop(columns=['Image-URL-S','Image-URL-M','Image-URL-L'],inplace=True)
+books.drop(columns=['Image-URL-S','Image-URL-M','Image-URL-L'],inplace=True)
 
 
 
@@ -43,44 +42,77 @@ book_user_ratings
 
 
 print("Split dataset")
-train,validate,test = np.split(book_user_ratings.sample(frac=1,random_state=42),[int(.6*len(book_user_ratings)),int(.8*len(book_user_ratings))])
-train.shape,validate.shape,test.shape
-train
+test,train = np.split(book_user_ratings.sample(frac=1,random_state=42),[int(.8*len(book_user_ratings))])
+test.shape,train.shape
 
 
 print("create pivot table")
-train_pivot = train.pivot(columns='User-ID',index=['Book-Title'],  values='Book-Rating').fillna(0)
-train_pivot.head()
+test_pivot = test.pivot(columns='User-ID',index=['Book-Title'],  values='Book-Rating').fillna(0)
+test_pivot.head()
 
 
 print("create sparse matrix")
-
 from scipy.sparse import csr_matrix
-train_csr =  csr_matrix(train_pivot)
+test_csr =  csr_matrix(test_pivot)
 
 
 print("apply knn")
 from sklearn.neighbors import NearestNeighbors
-model_knn = NearestNeighbors(metric='euclidean',algorithm='brute')
-model_knn.fit(train_csr)
+model_knn = NearestNeighbors(metric='euclidean',algorithm='auto')
+model_knn.fit(test_csr)
 
 
 
 print("Query away")
-title = list(train_pivot.index)
-
-def collector(query):
+title = list(test_pivot.index)
+# collaborative filtering
+def collab(query):
     output = []
     dist = []
     query_index=title.index(query)
-    distances, indices = model_knn.kneighbors(train_pivot.iloc[query_index,:].values.reshape(1,-1), n_neighbors = 6 )
+    distances, indices = model_knn.kneighbors(test_pivot.iloc[query_index,:].values.reshape(1,-1), n_neighbors = 6 )
     for i in range(1,len(indices.flatten())):
         if i == 0:
-            print("Recommendations for {0}:\n".format(train_pivot.index[indices.flatten()[i]]))
+            print("Recommendations for {0}:\n".format(test_pivot.index[indices.flatten()[i]]))
         if (i != 0):
-            output.append(train_pivot.index[indices.flatten()[i]])
+            output.append(test_pivot.index[indices.flatten()[i]])
             dist.append(distances.flatten()[i])
             out = pd.DataFrame({'knn-Distance':dist,'Book-Title':output})
     return out
 
+# content-based filtering
 
+books_title = test['Book-Title']
+books_title = books_title.drop_duplicates()
+book_title = books_title.reset_index(drop=True)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+from sklearn.metrics.pairwise import cosine_similarity
+
+tf = TfidfVectorizer(analyzer = 'word', ngram_range = (1,2), min_df = 0, stop_words = 'english')
+tfidf_matrix = tf.fit_transform(books_title)
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+cosine_sim
+
+indices = pd.Series(books_title.index,index = book_title)
+
+buks=pd.DataFrame()
+buks['Book-Title'] = book_title
+serie = pd.DataFrame()
+serie['series']=indices
+fin = pd.merge(buks,serie,on='Book-Title')
+
+def content(query):
+    idx = int(fin[fin['Book-Title']== query].index.values)
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key = lambda x:x[1], reverse = True)
+    sim_scores = sim_scores[1:5+1]
+    book_indices = [i[0] for i in sim_scores]
+
+    x=pd.DataFrame(sim_scores)
+    z=pd.DataFrame(buks.iloc[book_indices]).values
+    out=pd.DataFrame()
+    out['cosine_sim'],out['title'] = x[1],z
+    return out
+    
--- ui.py
+++ ui.py
@@ -3,10 +3,9 @@ import streamlit.components.v1 as components
 
 import pandas as pd
 import Book_Recommendation_system
-
 st.set_page_config(layout="wide")
 st.title("Book Recommendation System")
-st.subheader("Collaborative filtering using KNN")
+st.subheader("Hybrid Collaborative & Content filtering")
 
 title = st.text_input('Book TItle')
 
@@ -16,12 +15,20 @@ col1,col2 = st.columns((1,1))
 
 with col1:
     st.write("Book-titles")
-    st.dataframe(Book_Recommendation_system.train_pivot.index.unique())
+    st.dataframe(Book_Recommendation_system.book_title)
 with col2:
     if title and button:
-       output = Book_Recommendation_system.collector(title)
-       st.write("Recommendation for:\t",title)
-       #st.dataframe(Book_Recommendation_system.book_user_ratings)
-       #out = pd.DataFrame(output,columns=['Book-Title'])
-       st.dataframe(output)
+       colab_output = Book_Recommendation_system.collab(title)
+       cont_output = Book_Recommendation_system.content(title)
+       st.write("Recom based on Similar user ratings:\t",title)
+       st.dataframe(colab_output)
+       st.write("Recom based on Title similarity:\t",title)
+       st.dataframe(cont_output)
 components.html("""<center> BCSC01/0043/2018 &nbsp Barasa Mathews &nbsp &copy 2022 <center>""")
+
+       
+   
+
+
+
+
