4ca0a6694da37318ebf6d37d0c565279b0ed741c
Barasa Mathews <mathewdrucker@gmail.com>
2022-08-15 07:35:18 +0300

    Add files via upload

--- Book_Recommendation_system.py
+++ Book_Recommendation_system.py
@@ -16,15 +16,16 @@
 import numpy as np
 import pandas as pd
 
-books = pd.read_csv('book.csv', dtype='unicode')
-users = pd.read_csv('Users.csv')
-ratings = pd.read_csv('Ratings.csv')
+
+
+books = pd.read_csv('BX-Books.csv',sep=';',encoding='latin-1',on_bad_lines='skip',dtype='unicode')
+users = pd.read_csv('BX-Users.csv',sep=';',encoding='latin-1',on_bad_lines='skip')
+ratings = pd.read_csv('BX-Book-Ratings.csv',sep=';',encoding='latin-1',on_bad_lines='skip')
 
 print('drop 0 rated books')
 ratings = ratings[ratings['Book-Rating'] > 0]
 
 #books.drop(columns=['Image-URL-S','Image-URL-M','Image-URL-L'],inplace=True)
-
 
 
 book_ratings = pd.merge(books,ratings,on='ISBN').drop_duplicates()
@@ -40,49 +41,78 @@ print(book_user_ratings['Book-Title'].nunique(),'unique books' ,"\n" , book_user
 book_user_ratings = book_user_ratings.drop_duplicates(['User-ID'])
 book_user_ratings
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print('visualization')
+
+fig = plt.figure()
+dis = sns.displot(book_user_ratings['Book-Rating'])
+fig.savefig('rating.png')
+
+
+fig3=plt.figure()
+count=book_user_ratings.groupby('Book-Title')['Book-Rating'].count()
+avg=book_user_ratings.groupby('Book-Title')['Book-Rating'].mean()
+ax=sns.jointplot(x =count,y=avg)
+ax.set_axis_labels('number of ratings','rating')
+fig3.savefig('rater.png')
+
+fig4 = plt.figure()
+pp = sns.pairplot(book_user_ratings)
+fig4.savefig('rating.png')
+
+fig2 = plt.figure()
+users.Age.hist(bins=[0,10,20,30,40,50,60,70,100])
+plt.title('Age Distribution')
+plt.xlabel('Age')
+plt.ylabel('Count')
+fig2.savefig('age.png')
 
 print("Split dataset")
-test,train = np.split(book_user_ratings.sample(frac=1,random_state=42),[int(.8*len(book_user_ratings))])
-test.shape,train.shape
+train,test = np.split(book_user_ratings.sample(frac=1,random_state=42),[int(.8*len(book_user_ratings))])
+train.shape,test.shape
 
 
 print("create pivot table")
-test_pivot = test.pivot(columns='User-ID',index=['Book-Title'],  values='Book-Rating').fillna(0)
-test_pivot.head()
+train_pivot = train.pivot(columns='User-ID',index=['Book-Title'],values='Book-Rating').fillna(0)
+train_pivot.head()
 
 
 print("create sparse matrix")
 from scipy.sparse import csr_matrix
-test_csr =  csr_matrix(test_pivot)
+train_csr =  csr_matrix(train_pivot)
 
 
 print("apply knn")
 from sklearn.neighbors import NearestNeighbors
 model_knn = NearestNeighbors(metric='euclidean',algorithm='auto')
-model_knn.fit(test_csr)
+model_knn.fit(train_csr)
 
 
 
 print("Query away")
-title = list(test_pivot.index)
+title = list(train_pivot.index)
 # collaborative filtering
 def collab(query):
     output = []
     dist = []
     query_index=title.index(query)
-    distances, indices = model_knn.kneighbors(test_pivot.iloc[query_index,:].values.reshape(1,-1), n_neighbors = 6 )
+    distances, indices = model_knn.kneighbors(train_pivot.iloc[query_index,:].values.reshape(1,-1), n_neighbors = 6 )
     for i in range(1,len(indices.flatten())):
         if i == 0:
-            print("Recommendations for {0}:\n".format(test_pivot.index[indices.flatten()[i]]))
+            print("Recommendations for {0}:\n".format(train_pivot.index[indices.flatten()[i]]))
         if (i != 0):
-            output.append(test_pivot.index[indices.flatten()[i]])
+            output.append(train_pivot.index[indices.flatten()[i]])
             dist.append(distances.flatten()[i])
-            out = pd.DataFrame({'knn-Distance':dist,'Book-Title':output})
+            out = pd.DataFrame({'knn-Distance':dist,'title':output})
+            
+
     return out
 
 # content-based filtering
 
-books_title = test['Book-Title']
+books_title = train['Book-Title']
 books_title = books_title.drop_duplicates()
 book_title = books_title.reset_index(drop=True)
 
@@ -114,5 +144,8 @@ def content(query):
     z=pd.DataFrame(buks.iloc[book_indices]).values
     out=pd.DataFrame()
     out['cosine_sim'],out['title'] = x[1],z
+    out = out[out['cosine_sim'] > 0]
     return out
     
+
+
--- ui.py
+++ ui.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 import pandas as pd
-import Book_Recommendation_system
+import Book_Recommendation_system as brs
 st.set_page_config(layout="wide")
 st.title("Book Recommendation System")
 st.subheader("Hybrid Collaborative & Content filtering")
@@ -12,23 +12,47 @@ title = st.text_input('Book TItle')
 button=st.button("submit")
 
 col1,col2 = st.columns((1,1))
-
+col3,col4 = st.columns((9,1))
+col5,col6 = st.columns((1,1))
 with col1:
     st.write("Book-titles")
-    st.dataframe(Book_Recommendation_system.book_title)
-with col2:
-    if title and button:
-       colab_output = Book_Recommendation_system.collab(title)
-       cont_output = Book_Recommendation_system.content(title)
-       st.write("Recom based on Similar user ratings:\t",title)
-       st.dataframe(colab_output)
-       st.write("Recom based on Title similarity:\t",title)
-       st.dataframe(cont_output)
+    st.dataframe(brs.book_title)
+    
+#final content dataframe
+buku = brs.train.drop_duplicates(subset='Book-Title',keep='first')
+buku = buku.drop(['User-ID','Age','Location','Image-URL-M','Image-URL-L'],axis=1)
+
+if title and button:
+    colab_output = brs.collab(title)
+    cont_output = brs.content(title)
+    reco_list=list(colab_output['title']) + list(cont_output['title'])
+    
+        
+    with col2:
+        #collaborative filtering
+        st.write("Recom based on Similar user ratings:\t",title)
+        st.dataframe(colab_output)
+    
+        #content-based filtering
+        st.write("Recom based on Title similarity:\t",title)
+        st.dataframe(cont_output)
+    
+    with col3:
+    #output dataframe
+        st.write('Detailed Recommendation')
+        fin_df = buku[buku['Book-Title'].isin(reco_list)]
+        st.dataframe(fin_df.reset_index(drop=True))
+        
+    
+with col5:
+    st.write('Rating distribution')
+    st.pyplot(brs.dis)
+
+    
+with col6:
+    st.write('pairplot graphs')
+    st.pyplot(brs.pp)
+    
 components.html("""<center> BCSC01/0043/2018 &nbsp Barasa Mathews &nbsp &copy 2022 <center>""")
 
-       
    
-
-
-
-
